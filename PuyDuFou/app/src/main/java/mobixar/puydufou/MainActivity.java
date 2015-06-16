1aa4aa8b677266199e33d3bbc2e9aa4ed1bd83f5
Adrien <adrien@appsmiles.fr>
2015-06-16 11:04:00 +0200

    Application
    Création de l'application

--- PuyDuFou/app/src/main/java/mobixar/puydufou/MainActivity.java
+++ PuyDuFou/app/src/main/java/mobixar/puydufou/MainActivity.java
@@ -1,25 +1,23 @@
-package PuyDuFou.puydufou;
+package mobixar.puydufou;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import adrien.puydufou.R;
 
-
-public class Welcome extends Activity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
