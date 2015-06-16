e5ddb8d9d0c5ec3af6ed8a1af0b3c995a445768f
Adraca Asheart <adracaasheart@gmail.com>
2015-06-16 11:14:13 +0200

    Revert "Revert "Persistance""
    This reverts commit 9d85b6988644a672101126aa579650bd8c103057.

--- PDFBussinessService/src/java/com/pdf/entity/ActivityEntity.java
+++ PDFBussinessService/src/java/com/pdf/entity/ActivityEntity.java
@@ -1,5 +1,42 @@
 package com.pdf.entity;
 
-public class ActivityEntity {
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="activity")
+@Getter
+@Setter
+public class ActivityEntity implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="IDACTIVITY")
+    private Integer id;
+    
+    @Column(name="ACTIVITYNAME")
+    private String name;
+    
+    @Column(name="ACTIVITYDURATION")
+    private Integer duration;
+    
+    @Column(name="ACTIVITYCREATIONDATE")
+    private Date creationDate;
+   
+    @Column(name="ACTIVITYCAST")
+    private Integer cast;
+    
+    @Column(name="ACTIVITYSTORY")
+    private String story;
+    
+    public ActivityEntity(){
+    }
 }
